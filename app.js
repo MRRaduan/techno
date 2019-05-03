11fd291e9d2f3214ef19671f18fcfcd83f5b311d
adz.macb003 <adz.macb003@MacBook-Air-de-adzmacb003.local>
2019-05-03 17:59:54 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const vm = new Vue({
+	el: '#app',
+	data: {
+		produtos: [],
+		produto: ''
+	},
+	filters: {
+		numeroPreco(valor) {
+			return valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
+		}
+	},
+	methods: {
+		fetchProdutos() {
+			fetch('./api/produtos.json')
+				.then(r => r.json())
+				.then(r => this.produtos = r)
+		},
+		fetchProduto(id) {
+			fetch(`./api/produtos/${id}/dados.json`)
+				.then(r => r.json())
+				.then(r => this.produto = r)
+		}
+	},
+	created() {
+		this.fetchProdutos()
+	}
+})
